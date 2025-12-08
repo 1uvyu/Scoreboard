5f5b34b67aa5667e50f02ed46142b1f76fa98783
1uvyu <kayleec55@nycstudents.net>
2025-12-07 22:03:59 -0500

    defined recordPlay method and added test cases

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.sql.SQLOutput;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args) {
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(1);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(3);
+        info = game.getScore();
+        System.out.println(info);
+    }
 }
